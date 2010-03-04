9ca19e7c897b44f0ffdef0698b9ab094d1e7e2e2
Your Name <atomic.unit@gmail.com>
2010-03-04 08:12:22 -0800

    facebook testing

--- app/controllers/facebook/calculator_controller.rb
+++ app/controllers/facebook/calculator_controller.rb
@@ -1,6 +1,8 @@
 class Facebook::CalculatorController < ApplicationController
   ensure_application_is_installed_by_facebook_user
   
+  layout "facebook"
+  
   def index
     # render :text => facebook_session.user.name
   end
