786e8bf9c3e03dfa13c01d13289b19ff4b710374
Your Name <atomic.unit@gmail.com>
2010-03-04 07:28:41 -0800

    facebook fixes

--- app/controllers/facebook/calculator_controller.rb
+++ app/controllers/facebook/calculator_controller.rb
@@ -2,10 +2,6 @@ class Facebook::CalculatorController < ApplicationController
   ensure_application_is_installed_by_facebook_user
   
   def index
-    render :text => "ok"
-  end
-  
-  def show
     render :text => facebook_session.user.name
   end
 end
