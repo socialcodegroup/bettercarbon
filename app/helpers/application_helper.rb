3a32a32562efb47cd7ed7ced276e65d0a9b07cd9
Your Name <atomic.unit@gmail.com>
2010-03-04 08:09:20 -0800

    facebook testing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,18 @@ module ApplicationHelper
   def stylesheet_includes
     stylesheet_link_tag('reset', 'layout', :cache => "_stylesheet")
   end
+  
+  def facebook_javascript_includes
+    files = [
+              'jquery', 'jquery-ui', 'jrails', 'js-class', 'excanvas',
+              'bluff-min', 'swfobject', 'raphael-min', 'g.raphael/g.raphael-min',
+              'g.raphael/g.pie-min', 'g.raphael/g.bar', 'application', 'flot/jquery.flot.min',
+              'flot/jquery.flot.pie'
+            ]
+    javascript_include_tag(files, :cache => "_fB_javascript")
+  end
+  
+  def facebook_stylesheet_includes
+    stylesheet_link_tag('reset', 'fb_layout', :cache => "_fb_stylesheet")
+  end
 end
