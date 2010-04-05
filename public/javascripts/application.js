7d7cc43ddd1fa99485bad5af3759b2898b9a767a
Your Name <atomic.unit@gmail.com>
2010-04-05 00:41:42 -0700

    added some fb stuff

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -13,3 +13,15 @@ function replace_chart_with_chart_2(address) {
 	  }
 	});
 }
+function htmlentities(txt) {
+  var i,tmp,ret='';
+  for(i=0; i < txt.length; i++) {
+    tmp = txt.charCodeAt(i);
+    if( (tmp > 47 && tmp < 58) || (tmp > 62 && tmp < 127) ) {
+      ret += txt.charAt(i);
+    } else{
+      ret += "&#" + tmp + ";";
+    }
+  }
+  return ret;
+}
