a3ebd520e2bbb4c054501b53a79ff52de8927440
Your Name <atomic.unit@gmail.com>
2010-05-05 11:49:07 -0700

    renamed files

--- config/environment.rb
+++ config/environment.rb
@@ -32,7 +32,7 @@ Rails::Initializer.run do |config|
   config.gem "geokit"
   config.gem "peterpunk-mhash", :lib => 'mhash', :source => 'http://gems.github.com'
   config.gem "binarylogic-authlogic", :lib => 'authlogic', :source => "http://gems.github.com"
-  config.gem "facebooker"
+  # config.gem "facebooker"
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
