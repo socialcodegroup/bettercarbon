d9d1040030371b64d113ad2da23ca8a475da12ff
Your Name <atomic.unit@gmail.com>
2010-05-09 12:18:40 -0700

    testing fb

--- app/models/calculator_profile.rb
+++ app/models/calculator_profile.rb
@@ -1,15 +1,16 @@
 class CalculatorProfile
   def initialize
-    CarbonCalculator.modules.each do |mod|
-      mod.possible_inputs.each do |input|
-        instance_variable_set("@#{mod::REFINED_MODEL}_#{input[:name]}".to_sym, nil)
-      end
-    end
+    # CarbonCalculator.modules.each do |mod|
+    #   mod.possible_inputs.each do |input|
+    #     instance_variable_set("@#{mod::REFINED_MODEL}_#{input[:name]}".to_sym, nil)
+    #   end
+    # end
   end
   
   def method_missing(name, *args)
     # super unless name.to_s =~ /^w+_ismyname$/
     # puts "Why, hello #{name}!"
-    instance_variable_get("@#{name}".to_sym)
+    # instance_variable_get("@#{name}".to_sym)
+    nil
   end
 end
