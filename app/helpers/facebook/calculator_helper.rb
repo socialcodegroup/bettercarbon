bf4640f9097f2b85ed0d2d8bcf371fc8f8219200
Your Name <atomic.unit@gmail.com>
2010-05-09 12:16:53 -0700

    testing fb

--- app/helpers/facebook/calculator_helper.rb
+++ app/helpers/facebook/calculator_helper.rb
@@ -3,7 +3,7 @@ module Facebook::CalculatorHelper
     case input[:type]
     when :text_field
       # text_field_tag("profile[#{module_name.to_s}][#{input[:name]}]", params[:show_value]==false ? nil : number_with_precision(value, :precision => 2))
-      form.text_field "profile[#{module_name.to_s}][#{input[:name]}]", params[:show_value]==false ? nil : number_with_precision(value, :precision => 2), :label => input[:title]
+      form.text_field "profile[#{module_name.to_s}][#{input[:name]}]", :value => params[:show_value]==false ? nil : number_with_precision(value, :precision => 2), :label => input[:title]
     when :check_box
       check_box_tag("profile[#{module_name.to_s}][#{input[:name]}]", 1, (value + 0.5).to_i == 1)
     when :select
