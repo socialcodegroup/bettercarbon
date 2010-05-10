da624f95574a07e0fdf204a43ff136c30f58bca9
Your Name <atomic.unit@gmail.com>
2010-05-09 20:18:20 -0700

    more FB work

--- app/controllers/facebook/calculator_controller.rb
+++ app/controllers/facebook/calculator_controller.rb
@@ -14,6 +14,75 @@ class Facebook::CalculatorController < ApplicationController
   
   layout "facebook"
   
+  def hypertree_subtree
+    params[:calculator_profile] ||= {}
+
+    @calculator_input = CalculatorInput.new(:facebook => true, :fb_user_id => @facebook_session.user.uid)
+    @old_calculator_result = CarbonCalculator.process(@calculator_input)
+
+    old_mod_results = @old_calculator_result.per_module_results
+    stripped_values = strip_existing_values_from_refined_values(old_mod_results, params[:calculator_profile][:profile])
+
+    stripped_values.each do |mod, values|
+      mod = CarbonCalculator.modules.select{|c|c.to_s.gsub(':', '') == mod}.first
+      mod.process_input(@calculator_input, values)
+    end
+
+    @calculator_input.destroy_cache
+    @calculator_result = CarbonCalculator.process(@calculator_input)
+    @calculator_input.user_input.update_attribute(:algorithmic_footprint, @calculator_result.total_footprint)
+
+    average_footprint = Query.sum(:algorithmic_footprint) / Query.count(:all)
+
+    @gvalues = [
+        @calculator_result.total_footprint,
+        average_footprint,
+        5
+      ]
+
+    @gkeys = ["\'Your footprint\'", "\'Average Footprint\'", "'World'"]
+
+    # lookup footprint of friends (who have calculated their footprint)
+    @friends_with_app = facebook_session.user.friends_with_this_app
+
+    @friends_footprints = @friends_with_app.collect { |friend|
+      query = Query.find_by_facebook_uid(friend.uid)
+
+      if query
+        {
+          :friend => friend,
+          :footprint => query.algorithmic_footprint
+        }
+      else
+        nil
+      end
+    }.compact
+
+    footprints = @friends_footprints.collect { |friend_footprint| friend_footprint[:footprint] }
+    footprints = footprints + [@calculator_result.total_footprint]
+
+    @max = footprints.max
+
+    @friends_footprints_json = @friends_footprints.collect { |friend_footprint|
+      "{'data' : {'$color' : '#{CalcMath::number_to_intensity(friend_footprint[:footprint], 0, @max)}', '$dim' : #{friend_footprint[:footprint].to_i/1.5}},  'id' : '#{friend_footprint[:friend].uid}', 'name' : '#{friend_footprint[:friend].name} - #{sprintf('%.2f', friend_footprint[:footprint])}', 'children' : []}"
+      # #{sprintf('%.2f', friend_footprint[:footprint])}
+
+  # friend=<<FRIEND
+  #   {
+  #     'id' : '#{friend_footprint[:friend].uid}',
+  #     'name' : '#{friend_footprint[:friend].name}',
+  #     'children' : [],
+  #     'data' : {
+  #       '$aw' : 20,
+  #       '$color' : '#f55'
+  #     }
+  #   }
+  # FRIEND
+    }.join(',')
+
+    render :text => @friends_footprints_json
+  end
+  
   def framed_visualization
     params[:calculator_profile] ||= {}
 
