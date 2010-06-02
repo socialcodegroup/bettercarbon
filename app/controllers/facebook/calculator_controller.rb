1aa5769405c2e7019a7256b7cbdee56b99ea83cd
Your Name <atomic.unit@gmail.com>
2010-06-02 08:16:43 -0700

    working on fb fixes

--- app/controllers/facebook/calculator_controller.rb
+++ app/controllers/facebook/calculator_controller.rb
@@ -15,6 +15,54 @@ class Facebook::CalculatorController < ApplicationController
   
   layout "facebook"
   
+  def overview
+    @calculator_input = CalculatorInput.new(:facebook => true, :fb_user => @facebook_session.user)
+    @calculator_result = CarbonCalculator.process(@calculator_input)
+  end
+  
+  def graph
+    @calculator_input = CalculatorInput.new(:facebook => true, :fb_user => @facebook_session.user)
+    @calculator_result = CarbonCalculator.process(@calculator_input)
+
+    # lookup footprint of friends (who have calculated their footprint)
+    @friends_with_app = facebook_session.user.friends_with_this_app
+
+    @friends_footprints = @friends_with_app.collect { |friend|
+      query = Query.find_by_facebook_uid(friend.uid)
+
+      if query
+        {
+          :friend => friend,
+          :footprint => query.algorithmic_footprint
+        }
+      else
+        nil
+      end
+    }.compact
+
+    footprints = @friends_footprints.collect { |friend_footprint| friend_footprint[:footprint] }
+    footprints = footprints + [@calculator_result.total_footprint]
+
+    @max = footprints.max
+
+    @friends_footprints_json = @friends_footprints.collect { |friend_footprint|
+      "{'data' : {'$color' : '#{CalcMath::number_to_intensity(friend_footprint[:footprint], 0, @max)}', '$dim' : #{friend_footprint[:footprint].to_i/1.5}},  'id' : '#{friend_footprint[:friend].uid}', 'name' : '#{friend_footprint[:friend].name} - #{sprintf('%.2f', friend_footprint[:footprint])}', 'children' : []}"
+      # #{sprintf('%.2f', friend_footprint[:footprint])}
+
+  # friend=<<FRIEND
+  #   {
+  #     'id' : '#{friend_footprint[:friend].uid}',
+  #     'name' : '#{friend_footprint[:friend].name}',
+  #     'children' : [],
+  #     'data' : {
+  #       '$aw' : 20,
+  #       '$color' : '#f55'
+  #     }
+  #   }
+  # FRIEND
+    }.join(',')
+  end
+  
   def hypertree_subtree
     params[:calculator_profile] ||= {}
     
@@ -244,52 +292,6 @@ ROOTJSON
     
     average_footprint = Query.sum(:algorithmic_footprint) / Query.count(:all)
     
-    @gvalues = [
-        @calculator_result.total_footprint,
-        average_footprint,
-        5
-      ]
-    
-    @gkeys = ["\'Your footprint\'", "\'Average Footprint\'", "'World'"]
-    
-    # lookup footprint of friends (who have calculated their footprint)
-    @friends_with_app = facebook_session.user.friends_with_this_app
-    
-    @friends_footprints = @friends_with_app.collect { |friend|
-      query = Query.find_by_facebook_uid(friend.uid)
-      
-      if query
-        {
-          :friend => friend,
-          :footprint => query.algorithmic_footprint
-        }
-      else
-        nil
-      end
-    }.compact
-    
-    footprints = @friends_footprints.collect { |friend_footprint| friend_footprint[:footprint] }
-    footprints = footprints + [@calculator_result.total_footprint]
-    
-    @max = footprints.max
-    
-    @friends_footprints_json = @friends_footprints.collect { |friend_footprint|
-      "{'data' : {'$color' : '#{CalcMath::number_to_intensity(friend_footprint[:footprint], 0, @max)}', '$dim' : #{friend_footprint[:footprint].to_i/1.5}},  'id' : '#{friend_footprint[:friend].uid}', 'name' : '#{friend_footprint[:friend].name} - #{sprintf('%.2f', friend_footprint[:footprint])}', 'children' : []}"
-      # #{sprintf('%.2f', friend_footprint[:footprint])}
-      
-# friend=<<FRIEND
-#   {
-#     'id' : '#{friend_footprint[:friend].uid}',
-#     'name' : '#{friend_footprint[:friend].name}',
-#     'children' : [],
-#     'data' : {
-#       '$aw' : 20,
-#       '$color' : '#f55'
-#     }
-#   }
-# FRIEND
-    }.join(',')
-    
     if request.post?
     #   redirect_to('/bettercarbon/invites/new')
       begin
@@ -302,6 +304,8 @@ ROOTJSON
     end
     
     # FacebookerPublisher.deliver_templatized_news_feed(facebook_session.user)
+    
+    redirect_to('/bettercarbon/calculator/overview')
   end
   
   def publish_to_friends
